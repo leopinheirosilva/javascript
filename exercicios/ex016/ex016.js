b9007535567707e4bf4bbed8643c359893b2f309
leopinheirosilva <leonardopinheirosilva16@gmail.com>
2025-08-03 21:12:25 -0300

    Update ex016.js

--- exercicios/ex016/ex016.js
+++ exercicios/ex016/ex016.js
@@ -4,13 +4,14 @@ var min = hrAtual.getMinutes().toString().padStart(2, '0')
 var txtHora = document.getElementsByTagName('p')[0]
 var imagem = document.getElementById('imagem')
 txtHora.innerHTML = `Agora são ${hora}:${min} horas.`
+var hora = 22
 if (hora < 12) {
-    document.body.style.background = 'rgb(131, 182, 199)'
+    document.body.style.background = '#83B6C7'
     imagem.style.backgroundImage = 'url(midias/foto-dia.jpg)'
 } else if (hora < 19) {
-    document.body.style.background = 'rgb(173, 114, 74)'
+    document.body.style.background = '#A56737'
     imagem.style.backgroundImage = 'url(midias/foto-tarde.jpg)'
 } else {
-    document.body.style.background = '#2a2a2d'
+    document.body.style.background = '#1C272D'
     imagem.style.backgroundImage = 'url(midias/foto-noite.jpg)'
 }
