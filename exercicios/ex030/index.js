a3c5b212bbb53b14e43216aa361a754807498176
leopinheirosilva <leonardopinheirosilva16@gmail.com>
2025-08-05 20:01:58 -0300

    Update index.js

--- exercicios/ex030/index.js
+++ exercicios/ex030/index.js
@@ -1,5 +1,5 @@
 var amigo = {
-    nome: 'José', sexo: 'M', peso: 85.4, engordar(p = 0) {
+    nome: 'José', sexo: 'M', peso: 85.4, engordar(p) {
         console.log('Engordou')
         this.peso += p 
     }
