30566d6deef5aacfd326bb1e85edeca0912fe3c8
leopinheirosilva <leonardopinheirosilva16@gmail.com>
2025-08-05 18:19:02 -0300

    Update index.js

--- exercicios/ex025/index.js
+++ exercicios/ex025/index.js
@@ -1,9 +1,9 @@
-function parimpar(n) {
+function parimpar(n) {  //funçao recursiva
     if (n % 2 == 0) {
         return 'Par!'
     } else {
         return 'Impar!'
     }
 }
-let res = parimpar(220)
+let res = parimpar(20)
 console.log(res)
