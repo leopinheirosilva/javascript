13345819c14241a89a62e94bbafd75400661c2ec
leopinheirosilva <leonardopinheirosilva16@gmail.com>
2025-08-05 13:10:59 -0300

    Update index.js

--- exercicios/ex024/index.js
+++ exercicios/ex024/index.js
@@ -1,6 +1,6 @@
 let valores = [8, 1, 7, 4, 2, 9]
 for (let pos = 0; pos < valores.length; pos++) {
-    console.log(`${valores[pos]}`)
+    console.log(valores[pos])
 }
 for (let pos in valores) { // forma simplificada (funciona somente com arrays)
     console.log(valores[pos])
