087a18491402ca51d83ff6bd8a34daab5478e756
leopinheirosilva <leonardopinheirosilva16@gmail.com>
2025-08-08 17:31:31 -0300

    Update script.js

--- exercicios/ex032/script.js
+++ exercicios/ex032/script.js
@@ -1,3 +1,8 @@
 function limparConteudo() {
     alert('TESTE')
 }
+function criarConta() {
+    let botaoEnviar = document.getElementById('enviar')
+    let enviar = botaoEnvair.value
+    enviar.innerHTML = 'conta criada com sucesso!'
+}
