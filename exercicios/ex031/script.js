c358275a7373935b4e81ce7dda7bc97fb5ad5907
leopinheirosilva <leonardopinheirosilva16@gmail.com>
2025-08-07 23:06:53 -0300

    Update script.js

--- exercicios/ex031/script.js
+++ exercicios/ex031/script.js
@@ -12,12 +12,12 @@ function adicionarTarefa() {
     if (inputTarefa.value.length == 0 || inLista(inputTarefa.value, tarefaList)) {
         alert('[ERROR] Palavra inválida ou item já adicionado')
     } else {
-            let tarefa = inputTarefa.value
-            let lista = document.createElement('li')
-            tarefaList.push(tarefa)
-            inputTarefa.focus()
-            inputTarefa.value = ''
-            lista.textContent = tarefa
-            mensagem.appendChild(lista)
+        let tarefa = inputTarefa.value.trim()
+        let lista = document.createElement('li')
+        tarefaList.push(tarefa)
+        inputTarefa.focus()
+        inputTarefa.value = ''
+        lista.textContent = tarefa
+        mensagem.appendChild(lista)
     }
 }
