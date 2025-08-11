4f597e22843c8c9da6f32ed2162fb78f4764cdb9
leopinheirosilva <leonardopinheirosilva16@gmail.com>
2025-08-11 11:35:50 -0300

    Update script.js

--- exercicios/ex029/script.js
+++ exercicios/ex029/script.js
@@ -22,7 +22,7 @@ function adicionar() {
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
-        resultado.innerHTML = ''    //zera o conteúdo da variavel res sempre que um outro número é adicionado
+        resultado.innerHTML = ''    //zera o conteúdo da variavel resultado sempre que um outro número é adicionado
     } else {
         alert('Valor inválido ou já encontrado na lista.')
     }
