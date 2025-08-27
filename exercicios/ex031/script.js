55bd9354df59b90d65f52c929fbfc9fec5f0bbf4
leopinheirosilva <leonardopinheirosilva16@gmail.com>
2025-08-27 10:32:09 -0300

    code ident

--- exercicios/ex031/script.js
+++ exercicios/ex031/script.js
@@ -1,25 +1,25 @@
-const inputTarefa = document.getElementById('inputTarefa')
-const mensagem = document.getElementById('mensagem')
-const confirmTarefa = document.getElementById('confirmTarefa')
-let tarefaList = []
+const inputTarefa = document.getElementById("inputTarefa");
+const mensagem = document.getElementById("mensagem");
+const confirmTarefa = document.getElementById("confirmTarefa");
+let tarefaList = [];
 function inLista(n, lista) {
-    if (lista.indexOf(String(n)) != -1) {
-        return true
-    } else {
-        return false
-    }
+  if (lista.indexOf(String(n)) != -1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 function adicionarTarefa() {
-    if (inputTarefa.value.length == 0 || inLista(inputTarefa.value, tarefaList)) {
-        alert('[ERROR] Palavra inválida ou item já adicionado')
-    } else {
-        let tarefa = inputTarefa.value.trim()
-        let lista = document.createElement('li')
-        tarefaList.push(tarefa)
-        confirmTarefa.innerHTML = 'Tarefa adicionada!'
-        inputTarefa.focus()
-        inputTarefa.value = ''
-        lista.textContent = tarefa
-        mensagem.appendChild(lista)
-    }
+  if (inputTarefa.value.length == 0 || inLista(inputTarefa.value, tarefaList)) {
+    alert("[ERROR] Palavra inválida ou item já adicionado");
+  } else {
+    let tarefa = inputTarefa.value.trim();
+    let lista = document.createElement("li");
+    tarefaList.push(tarefa);
+    confirmTarefa.innerHTML = "Tarefa adicionada!";
+    inputTarefa.focus();
+    inputTarefa.value = "";
+    lista.textContent = tarefa;
+    mensagem.appendChild(lista);
+  }
 }
