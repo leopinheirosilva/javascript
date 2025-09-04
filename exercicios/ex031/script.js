78a43f5eafaff2454850548148af5d50087f115c
leopinheirosilva <leonardopinheirosilva16@gmail.com>
2025-09-04 16:49:18 -0300

    update ex031

--- exercicios/ex031/script.js
+++ exercicios/ex031/script.js
@@ -1,7 +1,8 @@
-const inputTarefa = document.getElementById("inputTarefa");
-const mensagem = document.getElementById("mensagem");
-const confirmTarefa = document.getElementById("confirmTarefa");
-let tarefaList = [];
+const inputTarefa = document.getElementById("inputTarefa"); //recebe os dados do input
+const mensagem = document.getElementById("mensagem"); //onde as tarefas serão exibidas
+const confirmTarefa = document.getElementById("confirmTarefa"); //texto de confirmação
+let tarefaList = []; //array que armazena as tarefas
+//verifica se a lista está vazia ou nao
 function inLista(n, lista) {
   if (lista.indexOf(String(n)) != -1) {
     return true;
@@ -9,20 +10,23 @@ function inLista(n, lista) {
     return false;
   }
 }
+//funcao para adicionar tarefa
 function adicionarTarefa() {
   let tarefa = inputTarefa.value.trim(); //remove os espaços entre a string
+  //verifica se o input está vazio ou se a tarefa já existe na lista
   if (tarefa == "" || inLista(tarefa, tarefaList)) {
     alert("[ERROR] Palavra inválida ou item já adicionado");
   } else {
     confirmTarefa.innerHTML = "Tarefa adicionada!";
-    tarefaList.push(tarefa);
+    tarefaList.push(tarefa); //adiciona a tarefa no array tarefaList
     renderizarTarefa(); //chama a funcao renderizarTarefa
   }
 }
 function renderizarTarefa() {
   mensagem.innerHTML = "";
+  //percorrendo o array tarefaList
   for (let i = 0; i < tarefaList.length; i++) {
-    let lista = document.createElement("li");
+    let lista = document.createElement("li"); //criando elemento li
     lista.textContent = tarefaList[i];
     //criando botao remover
     let botaoRemover = document.createElement("button");
@@ -33,33 +37,38 @@ function renderizarTarefa() {
     let botaoEditar = document.createElement("button");
     botaoEditar.className = "editar";
     botaoEditar.textContent = "editar";
-    botaoEditar.onclick = () => editarTarefa(i);
+    botaoEditar.onclick = () => editarTarefa(i); //chamando funcao editarTarefa
+    //adicionando os botoes na lista
     lista.appendChild(botaoRemover);
     lista.appendChild(botaoEditar);
+    //adicionando a lista na mensagem
     mensagem.appendChild(lista);
   }
-  inputTarefa.value = "";
-  inputTarefa.focus();
+  inputTarefa.value = ""; //limpa o input
+  inputTarefa.focus(); //foca o cursor no input
 }
 function removerTarefa(i) {
   tarefaList.splice(i, 1); //remove a tarefa da lista
   renderizarTarefa();
 }
 function editarTarefa(i) {
-  let tarefaEditada = prompt("Edite a tarefa:");
+  let tarefaEditada = prompt("Edite a tarefa:"); //caixa de prompt para editar a tarefa
+  //verifica se o input está vazio ou se a tarefa já existe na lista
   if (tarefaEditada.trim() == "" || inLista(tarefaEditada, tarefaList)) {
     alert("[ERROR] Palavra inválida ou item já adicionado");
   } else {
-    tarefaList[i] = tarefaEditada;
+    tarefaList[i] = tarefaEditada; //atualiza a tarefa no array
     renderizarTarefa();
   }
 }
+//funcao para limpar todo o conteudo da lista
 function limparConteudo() {
+  //verifica se a lista já está vazia
   if (tarefaList.length == 0) {
     alert("A lista já está vazia!");
   } else {
-    tarefaList.length = 0;
+    tarefaList.length = 0; //limpa o array tarefaList
     renderizarTarefa();
-    confirmTarefa.innerHTML = "Lista de tarefas limpa!";
+    confirmTarefa.innerHTML = "Lista de tarefas limpa!"; //mensagem de confirmação
   }
 }
